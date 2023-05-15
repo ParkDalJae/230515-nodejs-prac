3ee931773c99631ca385228e2e02e84b6973f2d3
ParkDalJae <dv.over931@gmail.com>
2023-05-15 14:31:45 +0900

    alert놀이

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-// 으아아
+const body = document.body;
+const btn = document.createElement("button");
+body.append(btn);
+
+btn.textContent = "click";
+btn.addEventListener("click", () => {
+  alert("집가고싶다");
+});
+body.style.display = "flex";
+body.style.height = "90vh";
+body.style.alignItems = "center";
+body.style.justifyContent = "center";
+
+btn.style.width = "400px";
+btn.style.height = "200px";
